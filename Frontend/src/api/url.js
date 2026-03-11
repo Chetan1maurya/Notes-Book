3306426e6b73e6af6c97ab8dc6ea7da60efaa6aa
Chetan Maurya <Escape>
2026-03-11 18:52:37 +0530

    Removed Authentication

--- Frontend/src/api/url.js
+++ Frontend/src/api/url.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const BACKEND_URL = axios.create({
-    baseURL:"https://notes-book-regm.onrender.com/notesapp"
+    baseURL:"https://notes-book-1.onrender.com/notesapp"
 })
 
 export default BACKEND_URL
